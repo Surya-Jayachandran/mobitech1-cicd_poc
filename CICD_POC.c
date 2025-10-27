d227a696d0bc4f20eba533d1a3f8095f958a3181
Surya Jayachandran <surya.j@mobitechwireless.com>
2025-10-27 14:41:28 +0530

    Initial commit - CICD POC setup

---
+++ CICD_POC.c
@@ -0,0 +1,85 @@
+/**************************************************************************//**
+ * @file     main.c
+ * @version  V3.00
+ * @brief    Show how to set GPIO pin mode and use pin data input/output control.
+ *
+ * @copyright (C) 2013~2015 Nuvoton Technology Corp. All rights reserved.
+ *
+ ******************************************************************************/
+#include "stdio.h"
+#include "NuMicro.h"
+
+
+#define PLL_CLOCK       192000000
+
+
+void SYS_Init(void)
+{
+
+    /* Set XT1_OUT(PF.2) and XT1_IN(PF.3) to input mode */
+    PF->MODE &= ~(GPIO_MODE_MODE2_Msk | GPIO_MODE_MODE3_Msk);
+
+    /* Enable HXT clock (external XTAL 12MHz) */
+    CLK_EnableXtalRC(CLK_PWRCTL_HXTEN_Msk);
+
+    /* Wait for HXT clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk);
+
+    /* Set core clock as PLL_CLOCK from PLL */
+    CLK_SetCoreClock(PLL_CLOCK);
+
+    /* Set PCLK0/PCLK1 to HCLK/2 */
+    CLK->PCLKDIV = (CLK_PCLKDIV_APB0DIV_DIV2 | CLK_PCLKDIV_APB1DIV_DIV2);
+
+    /* Enable UART module clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+
+    /* Select UART module clock source as HXT and UART module clock divider as 1 */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HXT, CLK_CLKDIV0_UART0(1));
+
+    /* Set GPB multi-function pins for UART0 RXD and TXD */
+    SYS->GPB_MFPH &= ~(SYS_GPB_MFPH_PB12MFP_Msk | SYS_GPB_MFPH_PB13MFP_Msk);
+    SYS->GPB_MFPH |= (SYS_GPB_MFPH_PB12MFP_UART0_RXD | SYS_GPB_MFPH_PB13MFP_UART0_TXD);
+
+}
+
+void UART0_Init()
+{
+    /* Configure UART0 and set UART0 baud rate */
+    UART_Open(UART0, 115200);
+}
+
+int32_t main(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Init System, peripheral clock and multi-function I/O */
+    SYS_Init();
+
+    /* Lock protected registers */
+    SYS_LockReg();
+
+    /* Init UART0 for printf */
+    UART0_Init();
+
+    printf("\n\nCPU @ %dHz\n", SystemCoreClock);
+
+    printf("+-------------------------------------------------+\n");
+    printf("|    PB.3(Output) and PD.8(Input) Sample Code     |\n");
+    printf("+-------------------------------------------------+\n\n");
+
+  
+    /* Configure PB.3 as Output mode and PD.8 as Input mode then close it */
+    GPIO_SetMode(PB, BIT3, GPIO_MODE_OUTPUT);
+    GPIO_SetMode(PD, BIT8, GPIO_MODE_INPUT);
+
+    printf("GPIO PB.3(output mode) connect to PD.8(input mode) ......");
+
+  
+
+    while(1)
+		{
+				printf("Hello");
+		}
+}
